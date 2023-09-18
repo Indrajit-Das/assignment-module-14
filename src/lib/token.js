0b7ab5188394d42d65e96ef51176d710294871ea
Indrajit-Das <indrojit210210@gmail.com>
2023-09-18 20:23:01 +0600

    change in deployment domain name

--- src/lib/token.js
+++ src/lib/token.js
@@ -6,7 +6,7 @@ export async function generateToken(payload){
     let token = await new SignJWT(payload)
             .setProtectedHeader({alg:'HS256'})
             .setIssuedAt()
-            .setIssuer("https://assignment-module-14-psi.vercel.app")
+            .setIssuer("https://assignment-module-14-3dzzvxhd5-indrojit70.vercel.app")
             .setExpirationTime('2h')
             .sign(secretKey);
 
